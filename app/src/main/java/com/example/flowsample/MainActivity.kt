14c030ac51c9c801ccfe4f243b657297fe2b8e88
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-03-08 00:37:42 +0900

    implement ConflatedBroadcastChannel

--- app/src/main/java/com/example/flowsample/MainActivity.kt
+++ app/src/main/java/com/example/flowsample/MainActivity.kt
@@ -6,12 +6,9 @@ import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
-import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,36 +16,81 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var job = startCollect()
-
-        button.setOnClickListener {
-            Log.d("onClick:", "isActive?: ${job.isActive}, isCancelled?: ${job.isCancelled}")
-            if (job.isActive) job.cancel()
-            else job = startCollect()
+        var flowJob = startCollectFlow()
+        flowButton.setOnClickListener {
+            if (flowJob.isActive) flowJob.cancel()
+            else flowJob = startCollectFlow()
         }
+
+        var channelJob = startCollectChannel()
+        offerUnlimitedChannel() // channel job executed independently.
+        channelButton.setOnClickListener {
+            if (channelJob.isActive) channelJob.cancel()
+            else channelJob = startCollectChannel()
+        }
+
     }
 
-    private fun startCollect(): Job {
+    /*---------flow-----------*/
+    private fun startCollectFlow(): Job {
         return lifecycleScope.launch {
-            makeUnlimitedFlow()
-                    .catch {
-                        Toast.makeText(this@MainActivity, "failure", Toast.LENGTH_LONG).show()
-                    }
-                    .collect { elem ->
-                        text_view.text = elem
-                    }
+            unlimitedFlow
+                .catch {
+                    Toast.makeText(this@MainActivity, "failure", Toast.LENGTH_LONG).show()
+                }
+                .collect { elem ->
+                    flowConsumerText.text = "flow consume: $elem"
+                }
         }
     }
 
-    private fun makeUnlimitedFlow() = flow {
+    private val unlimitedFlow  = flow {
         var count = 0
         while (true) {
             emit(count)
-            if (count > 0 && count % 10 == 0) throw RuntimeException()
+//            if (count > 0 && count % 10 == 0) throw RuntimeException() // check error handling
             Log.d("flow elem:", "$count")
             count++
             delay(500)
         }
-    }.map { it.toString() }
-            .flowOn(Dispatchers.IO)
+    }.flowOn(Dispatchers.IO)
+        .map {
+            flowProducerText.text = "flow produce: $it"
+            it
+        }
+        .flowOn(Dispatchers.Main)
+
+    /*---------flow-----------*/
+
+
+
+    /*---------channel-----------*/
+    private fun startCollectChannel(): Job {
+        return lifecycleScope.launch {
+            channel.asFlow()
+                .catch {
+                    Toast.makeText(this@MainActivity, "failure", Toast.LENGTH_LONG).show()
+                }
+                .collect { elem ->
+                    channelConsumerText.text = "channel consume: $elem"
+                }
+        }
+    }
+
+    private val channel = ConflatedBroadcastChannel<Int>()
+    private fun offerUnlimitedChannel() {
+        lifecycleScope.launch(Dispatchers.Default) {
+            var count = 0
+            while (true) {
+                channel.offer(count)
+                count++
+                withContext(Dispatchers.Main) {
+                    channelProducerText.text = "channel produce: $count"
+                }
+                Log.d("channel elem", "$count")
+                delay(500L)
+            }
+        }
+    }
+    /*---------channel-----------*/
 }
